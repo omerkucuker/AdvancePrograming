aca61b2fecdaedba4a29d549636861db160787f8
Omer Kucuker <omerfaruk.kucuker@stu.fsm.edu.tr>
2020-12-12 15:41:22 +0300

    update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,51 @@
-const CACHE ='AP'
+const CACHE = "AP";
 function installCB(e) {
   console.log(CACHE, e);
 }
-addEventListener('install', installCB)
+
+addEventListener("install", installCB);
 
 function save(req, resp) {
-  if (!req.url.includes("github")) 
-     return resp;
-  return caches.open(CACHE)
-  .then(cache => { // save request
-    cache.put(req, resp.clone());
-    return resp;
-  }) 
-  .catch(console.err)
+  if (!req.url.includes("github")) return resp;
+  return caches
+    .open(CACHE)
+    .then(cache => {
+      // save request
+      cache.put(req, resp.clone());
+      return resp;
+    })
+    .catch(console.err);
 }
+
 function report(req) {
-  console.log(CACHE+' matches '+req.url)
-  return req
+  console.log(CACHE + " matches " + req.url);
+  return req;
 }
-function fetchCB(e) { //fetch first
-  let req = e.request
+
+function cacheCB(e) { //cache first
+    let req = e.request
+    e.respondWith(
+      caches.match(req)
+      .then(r1 => r1 || fetch(req))
+      .catch(console.log)
+    )
+  }
+  self.addEventListener('fetch', cacheCB)
+
+function fetchCB(e) {
+  //fetch first
+  let req = e.request;
   e.respondWith(
-    fetch(req).then(r2 => save(req, r2))
-    .catch(() => caches.match(req).then(report))
-  )
+    fetch(req)
+      .then(r2 => save(req, r2))
+      .catch(() => caches.match(req).then(report))
+  );
 }
-addEventListener('fetch', fetchCB)
+
+addEventListener("fetch", fetchCB);
 
 function activateCB(e) {
   console.log(CACHE, e);
 }
-addEventListener('activate', activateCB);
 
+addEventListener("activate", activateCB);
