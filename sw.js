7990f8b179664d167fda3488eb1065b7e0285f05
Omer Kucuker <omerfaruk.kucuker@stu.fsm.edu.tr>
2020-12-12 21:10:26 +0300

    update sw.js

--- sw.js
+++ sw.js
@@ -22,30 +22,16 @@ function report(req) {
   return req;
 }
 
-function cacheCB(e) { //cache first
-    let req = e.request
-    e.respondWith(
-      caches.match(req)
-      .then(r1 => r1 || fetch(req))
-      .catch(console.log)
-    )
-  }
-  self.addEventListener('fetch', cacheCB)
-
-function fetchCB(e) {
-  //fetch first
-  let req = e.request;
+function fetchCB(e) { //fetch first
+  let req = e.request
   e.respondWith(
-    fetch(req)
-      .then(r2 => save(req, r2))
-      .catch(() => caches.match(req).then(report))
-  );
+    fetch(req).then(r2 => save(req, r2))
+    .catch(() => caches.match(req).then(report))
+  )
 }
-
-addEventListener("fetch", fetchCB);
+addEventListener('fetch', fetchCB)
 
 function activateCB(e) {
   console.log(CACHE, e);
 }
-
-addEventListener("activate", activateCB);
+addEventListener('activate', activateCB);
