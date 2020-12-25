291cd4835eac8b88e3df77894749078f2571eab5
Omer Kucuker <omerfaruk.kucuker@stu.fsm.edu.tr>
2020-12-25 22:12:13 +0300

    update cache name

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'AP';
+const CACHE = 'AdvancePrograming';
 const FILES = ['AdvancePrograming/images/icon192', 'AdvancePrograming/TermProject/KelimeTahmin.html', 'AdvancePrograming/CW%232/Counting.html','AdvancePrograming/CW%232/length.html','AdvancePrograming/CW%233/chap4.html','AdvancePrograming/CW%234/List_of_Objects.html','AdvancePrograming/CW%235/chap4.html','AdvancePrograming/CW%236/Callback_and_Promise.html','AdvancePrograming/CW%237/StudentsData.html','AdvancePrograming/CW%238/FilesSelect.html','AdvancePrograming/CW%239/cw9_Page_by_JS.html','AdvancePrograming/CW%2310/Crypto_cw10.html','AdvancePrograming/CW%2311/Event_listeners.html','AdvancePrograming/CW%2312/GitHubApi.html','AdvancePrograming/CW%2313/WeatherApi.html','AdvancePrograming/CW%2314/Open_Maps_with_WeatherApi.html','AdvancePrograming/CW%2315/DriveAPI.html','AdvancePrograming/HW1/Poisson_table.html','AdvancePrograming/HW2/StudentsDB.html','305/anim/Animation.html']
 function installCB(e) {
   console.log(CACHE, e);
