a0f9d0afb8901612392c30bbfb0089683f01462f
Omer Kucuker <omerfaruk.kucuker@stu.fsm.edu.tr>
2020-12-27 14:08:10 +0300

    sw.js file

--- sw.js
+++ sw.js
@@ -7,12 +7,10 @@ addEventListener('install', installCB)
 function save(req, resp) {
   if (!req.url.includes("github")) 
      return resp;
-  return caches.open(CACHE)
-  .then(cache => { // save request
-    cache.put(req, resp);
-    return resp.clone();
-  }) 
-  .catch(console.err)
+  return caches.open(CACHE).then(cache => { // save request
+    cache.put(req, resp.clone());
+    return resp;
+  }).catch(console.err)
 }
 function report(req) {
   console.log(CACHE+' matches '+req.url)
@@ -21,8 +19,8 @@ function report(req) {
 function fetchCB(e) { //fetch first
   let req = e.request
   e.respondWith(
-    fetch(req).then(r2 => save(req, r2))
-    .catch(() => caches.match(req).then(report))
+    fetch(req).then(r2 => save(req, r2)).catch(
+	() => caches.match(req).then(report))
   )
 }
 addEventListener('fetch', fetchCB)
