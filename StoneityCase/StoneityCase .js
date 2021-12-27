11683226be90fca5aad48c130ca78c19c2643331
Omer Kucuker <omerfaruk.kucuker@stu.fsm.edu.tr>
2021-12-27 22:52:22 +0300

    case solution

---
+++ StoneityCase/StoneityCase .js
@@ -0,0 +1,16 @@
+const points4 = [16, 23, 7, 11, 3, 14];
+const points5 =[4, 13, 8, 9, 7, 1]
+
+function descendingSort(list){
+let result = [];
+let listDescending = list.sort(function(a,b){return b-a});
+let len= parseInt(list.length/2)
+let j=0;
+for(let i=0;i<len;i++){
+result[i]=listDescending[j];
+result[list.length-i-1]=listDescending[j+1];
+j+=2;
+}
+return result;
+}
+console.log(descendingSort(points5));
